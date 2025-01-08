078fcb3390f6fa22a16a74dc641a31fbfab4b415
ji3cp4 <107534806+ji3cp4@users.noreply.github.com>
2025-01-09 00:24:55 +0800

    version1

---
+++ Design/js/loginj.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $(".touch").click(function(){
+        $('#home').show();
+        $('#myself').show();
+        $('#piece').show();
+        $('#experience').show();
+     })
+});
+
+// var box = document.querySelector(".touch");
+// box.addEventListener("mousemove", touch, false);
+// function touch() {
+//      alert("滑鼠碰到我了");
+// }
+
